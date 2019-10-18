01732fdda534490b51887f9094d0a7da2c5192bf
Jianfeng XU <jxu@maltem.com>
2019-10-18 16:27:44 +0200

    Fix es6 style

--- es6/async-await.js
+++ es6/async-await.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const readFile = function(fileName) {
-  return new Promise(function(resolve, reject) {
+const readFile = function (fileName) {
+  return new Promise(((resolve, reject) => {
     fs.readFile(fileName, function(error, data) {
       if (error) return reject(error);
       resolve(data);
     });
-  });
+  }));
 };
 
-const gen = function*() {
-  const f1 = yield readFile("./var/test.txt");
-  const f2 = yield readFile("./var/test1.txt");
+const gen = function () {
+  const f1 = readFile('./var/test.txt');
+  const f2 = readFile('./var/test1.txt');
   console.log(f1.toString());
   console.log(f2.toString());
 };
--- node/helloworld.js
+++ node/helloworld.js
@@ -1,6 +1,6 @@
-var http = require('http')
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-type': 'text/plain'})
-  res.end('Hello world\n')
-}).listen(8889, '127.0.0.1')
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+  res.end('Hello world\n');
+}).listen(8889, '127.0.0.1');
