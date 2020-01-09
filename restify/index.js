d0617639d35f0e4994f4cf23037eba6c49118ffc
fengdi <xujianfen@gmail.com>
2020-01-09 22:48:13 +0100

    add eslint

--- restify/index.js
+++ restify/index.js
@@ -1,13 +1,18 @@
-const restify = require("restify")
+const restify = require('restify');
 
-function respond (req, res, next) {
-    res.send('hello ' + req.params.name);
-    next();
+function respond(req, res, next) {
+  res.send(`hello ${req.params.name}`);
+  next();
 }
 
-var server = restify.createServer({name: "demo"});
+const server = restify.createServer({ name: 'demo' });
 
-server.pre(() => console.log("-- Call pre handler"));
+server.pre(() => {
+  console.log('-- Call pre handler');
+});
+
+// TODO: Debug not work if added use hook
+
 // server.use((req, res, next) => {
 //     console.log("-- Call use handler");
 //     return next();
@@ -16,6 +21,6 @@ server.pre(() => console.log("-- Call pre handler"));
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function () {
-    console.log("%s listening at %s", server.name, server.url);
+server.listen(8080, () => {
+  console.log('%s listening at %s', server.name, server.url);
 });
